fcf33867372bf394853b2236a8200087a7a27540
鹿文龙 <90.wenlong.lu@gmail.com>
2019-08-19 18:56:09 +0800

    Feat : rxJava 拆分

--- app/src/main/java/com/cn/demo/transformer/CacheTransformer.java
+++ app/src/main/java/com/cn/demo/transformer/CacheTransformer.java
@@ -1,10 +1,13 @@
 package com.cn.demo.transformer;
 
-import java.lang.reflect.Field;
+import org.reactivestreams.Publisher;
 
+import io.reactivex.Flowable;
+import io.reactivex.FlowableTransformer;
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
+import okhttp3.Request;
 
 /**
  * Date: 2019-08-19
@@ -13,16 +16,13 @@ import io.reactivex.ObservableTransformer;
  * <p>
  * author: 鹿文龙
  * <p>
- * 这里使用了 https://github.com/yale8848/RetrofitCache 中的源码
  */
 public class CacheTransformer {
 
 	private static final String CLASS_NAME1 = "retrofit2.adapter.rxjava2.BodyObservable";
 	private static final String CLASS_NAME2 = "retrofit2.adapter.rxjava2.ResultObservable";
-	private static final String CLASS_NAME3 = "retrofit2.adapter.rxjava2.CallEnqueueObservable";
-	private static final String CLASS_NAME4 = "retrofit2.adapter.rxjava2.CallExecuteObservable";
 
-	public static <T> ObservableTransformer<T, T> emptyTransformer() {
+	public static <T> ObservableTransformer<T, T> obsTransformer() {
 		return new ObservableTransformer<T, T>() {
 			@Override
 			public ObservableSource<T> apply(io.reactivex.Observable<T> upstream) {
@@ -35,59 +35,24 @@ public class CacheTransformer {
 		};
 	}
 
+	public static <T> FlowableTransformer<T, T> flowTransformer() {
+		return new FlowableTransformer<T, T>() {
+			@Override
+			public Publisher<T> apply(Flowable<T> upstream) {
+				String name = upstream.getClass().getName();
+				if (name.equals(CLASS_NAME1) || name.equals(CLASS_NAME2)) {
+					flowable(upstream);
+				}
+				return upstream;
+			}
+		};
+	}
+
 	private static <T> void observable(Observable<T> observable) {
+		Request request = new GenerateRequest().generateRequest(observable);
+	}
 
-		try {
-			Object serviceMethodObj = null;
-			Object[] args;
-			Field upstream = observable.getClass().getDeclaredField("upstream");
-			upstream.setAccessible(true);
-
-			Object obj = upstream.get(observable);
-
-			Class cls = null;
-			if (obj.getClass().getName().equals(CLASS_NAME3)) {
-				cls = Class.forName(CLASS_NAME3);
-
-			} else if (obj.getClass().getName().equals(CLASS_NAME4)) {
-				cls = Class.forName(CLASS_NAME4);
-			}
-			if (cls == null) {
-				return;
-			}
-			Field foriginalCall = cls.getDeclaredField("originalCall");
-			foriginalCall.setAccessible(true);
-			Object OkhttpCallObj = foriginalCall.get(obj);
-
-			Class clsOkhttpCall = Class.forName("retrofit2.OkHttpCall");
-			Field fdArgs = clsOkhttpCall.getDeclaredField("args");
-
-
-			fdArgs.setAccessible(true);
-			args = (Object[]) fdArgs.get(OkhttpCallObj);
-
-			Field fdserviceMethod = null;
-			try {
-				fdserviceMethod = clsOkhttpCall.getDeclaredField("serviceMethod");
-			} catch (Exception e) {
-
-			}
-			if (fdserviceMethod == null) {
-				Field filedRequestFactory = clsOkhttpCall.getDeclaredField("requestFactory");
-				filedRequestFactory.setAccessible(true);
-				serviceMethodObj = filedRequestFactory.get(OkhttpCallObj);
-
-			} else {
-				fdserviceMethod.setAccessible(true);
-				serviceMethodObj = fdserviceMethod.get(OkhttpCallObj);
-			}
-
-			if (serviceMethodObj != null) {
-				GenerateRequest generateRequest = new GenerateRequest();
-				generateRequest.buildRequest(serviceMethodObj, args);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private static <T> void flowable(Flowable<T> flowable) {
+		Request request = new GenerateRequest().generateRequest(flowable);
 	}
 }
--- app/src/main/java/com/cn/demo/transformer/GenerateRequest.java
+++ app/src/main/java/com/cn/demo/transformer/GenerateRequest.java
@@ -3,6 +3,8 @@ package com.cn.demo.transformer;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
 import okhttp3.Request;
 
 /**
@@ -18,7 +20,97 @@ import okhttp3.Request;
  */
 public class GenerateRequest {
 
-	public Request buildRequest(Object serviceMethod, Object[] args) throws Exception {
+	private static final String CLASS_NAME3 = "retrofit2.adapter.rxjava2.CallEnqueueObservable";
+	private static final String CLASS_NAME4 = "retrofit2.adapter.rxjava2.CallExecuteObservable";
+
+	public <T> Request generateRequest(Flowable<T> flowable) {
+		Request request = null;
+		try {
+			Field upstream = flowable.getClass().getDeclaredField("upstream");
+			upstream.setAccessible(true);
+			Object obj = upstream.get(flowable);
+			request = objectGenerateRequest(obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return request;
+	}
+
+	public <T> Request generateRequest(Observable<T> observable) {
+		Request request = null;
+		try {
+			Field upstream = observable.getClass().getDeclaredField("upstream");
+			upstream.setAccessible(true);
+			Object obj = upstream.get(observable);
+			request = objectGenerateRequest(obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return request;
+	}
+
+	/**
+	 * Object 转 Request
+	 *
+	 * @param obj
+	 * @return
+	 * @throws Exception
+	 */
+	private Request objectGenerateRequest(Object obj) throws Exception {
+		Object serviceMethodObj = null;
+		Object[] args;
+		Class cls = null;
+		if (obj.getClass().getName().equals(CLASS_NAME3)) {
+			cls = Class.forName(CLASS_NAME3);
+
+		} else if (obj.getClass().getName().equals(CLASS_NAME4)) {
+			cls = Class.forName(CLASS_NAME4);
+		}
+		if (cls == null) {
+			return null;
+		}
+		Field foriginalCall = cls.getDeclaredField("originalCall");
+		foriginalCall.setAccessible(true);
+		Object OkhttpCallObj = foriginalCall.get(obj);
+
+		Class clsOkhttpCall = Class.forName("retrofit2.OkHttpCall");
+		Field fdArgs = clsOkhttpCall.getDeclaredField("args");
+
+
+		fdArgs.setAccessible(true);
+		args = (Object[]) fdArgs.get(OkhttpCallObj);
+
+		Field fdserviceMethod = null;
+		try {
+			fdserviceMethod = clsOkhttpCall.getDeclaredField("serviceMethod");
+		} catch (Exception e) {
+
+		}
+		if (fdserviceMethod == null) {
+			Field filedRequestFactory = clsOkhttpCall.getDeclaredField("requestFactory");
+			filedRequestFactory.setAccessible(true);
+			serviceMethodObj = filedRequestFactory.get(OkhttpCallObj);
+
+		} else {
+			fdserviceMethod.setAccessible(true);
+			serviceMethodObj = fdserviceMethod.get(OkhttpCallObj);
+		}
+
+		if (serviceMethodObj != null) {
+			return buildRequest(serviceMethodObj, args);
+		}
+		return null;
+	}
+
+	/**
+	 * 生成 Request
+	 *
+	 * @param serviceMethod
+	 * @param args
+	 * @return
+	 * @throws Exception
+	 */
+	private Request buildRequest(Object serviceMethod, Object[] args) throws Exception {
 		String objName = serviceMethod.getClass().getName();
 		Method toRequestMethod = null;
 
